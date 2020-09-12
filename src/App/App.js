d742260bb658db35f29ff9a515f782755344b86e
Julian <julimerlo95@gmail.com>
2020-09-12 16:23:22 -0300

    fix: update navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Loading } from '../components/util/loading/Loading';
-const Navbar = lazy(() => import("../components/navbar/Navbar"));
 const Footer = lazy(() => import("../components/footer/Footer"));
 const Content = lazy(() => import("../components/content/Content"));
 const RenderContent = lazy(() =>
